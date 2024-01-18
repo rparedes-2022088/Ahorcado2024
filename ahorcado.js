d1e485309a02fd9be85ab77d9e30b8ffad5d6b59
rparedes-2022088 <rparedes-2022088@kinal.edu.gt>
2024-01-18 16:36:41 -0600

    segundo push

---
+++ ahorcado.js
@@ -0,0 +1,5 @@
+addEventListener
+let abecedarioMin = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let abecedarioMay = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","o","P","Q","R","S","T","U","V","W","X","Y","Z"];
+alert(abecedarioMin[1]);
+
